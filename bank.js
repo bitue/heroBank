dfbcdefe0e8073c5d2c5826901f228476980387c
bitueboss <69850346+bitueboss@users.noreply.github.com>
2021-08-17 02:33:04 +0600

    added some fun

--- bank.js
+++ bank.js
@@ -3,34 +3,65 @@ let btnwithdraw = document.getElementById("withdraw-btn")
 let btndeposit = document.getElementById("deposit-btn")
 
 //add event withdraw 
+function inputToValue(inputId){
+    let value = document.getElementById(inputId).value
+    //need to parseFloat this value 
+    value = parseFloat(value)
+    if (isNaN(value)) {
+        document.getElementById(inputId).value = ""
+         alert("please Enter correct amount")
+         return false
+    }
+    if (value < 0) {
+        document.getElementById(inputId).value = ""
+         alert("please enter correct amount")
+         return false
+    }
+    return value
+
+}
+function textToValue(textId ,result){
+    if(result==false){
+        return alert("enter the correct amount")
+    }
+    let grabID = document.getElementById(textId)
+    let current = grabID.innerText
+    current = parseFloat(current)
+    return current
+
+}
 
 btndeposit.addEventListener("click", function () {
     //input value from the deposit field
-    let depositValue = document.getElementById("deposit").value
+    // let depositValue = document.getElementById("deposit").value
     //need to parseFloat this value 
-    depositValue = parseFloat(depositValue)
+    depositValue = inputToValue("deposit")
     //add some breakpoints 
-    if (isNaN(depositValue)) {
-        document.getElementById("deposit").value = ""
-        return alert("please Enter correct amount")
-    }
-    if (depositValue < 0) {
-        document.getElementById("deposit").value = ""
-        return alert("please enter correct amount")
-    }
+    // if (isNaN(depositValue)) {
+    //     document.getElementById("deposit").value = ""
+    //     return alert("please Enter correct amount")
+    // }
+    // if (depositValue < 0) {
+    //     document.getElementById("deposit").value = ""
+    //     return alert("please enter correct amount")
+    // }
 
     //grab the deposit amount show span tag
-    let currentDeposit = document.getElementById("deposit-amount")
+    // let currentDeposit = document.getElementById("deposit-amount")
     //to get value by innerText 
-    let currentDepositValue = currentDeposit.innerText
+    // let currentDepositValue = currentDeposit.innerText
     //need to parseFloat this value
-    currentDepositValue = parseFloat(currentDepositValue)
+    // currentDepositValue = parseFloat(currentDepositValue)
     //new deposit value = prev deposit value + current deposit value 
-
+    currentDepositValue = textToValue("deposit-amount",depositValue)
+    if(currentDepositValue==undefined){
+        return 
+    }
+   
     depositValue = depositValue + currentDepositValue
     //set the value by innerText
 
-    currentDeposit.innerText = depositValue
+    document.getElementById("deposit-amount").innerText = depositValue
     //update the balance amount 
     let balance = document.getElementById("balance").innerText
     balance = parseFloat(balance)
@@ -44,19 +75,19 @@ btndeposit.addEventListener("click", function () {
 
 btnwithdraw.addEventListener("click", function () {
     //input field value as withdraw
-    let withdrawValue = document.getElementById("withdraw").value
-    withdrawValue = parseFloat(withdrawValue)
+    // let withdrawValue = document.getElementById("withdraw").value
+    withdrawValue = inputToValue("withdraw")
     //need to get the current balance
     let balance = document.getElementById("balance").innerText
     balance = parseFloat(balance)
-    if (isNaN(withdrawValue)) {
-        document.getElementById("withdraw").value = ""
-        return alert("please Enter correct amount")
-    }
-    if (withdrawValue < 0) {
-        document.getElementById("withdraw").value = ""
-        return alert("please enter correct amount")
-    }
+    // if (isNaN(withdrawValue)) {
+    //     document.getElementById("withdraw").value = ""
+    //     return alert("please Enter correct amount")
+    // }
+    // if (withdrawValue < 0) {
+    //     document.getElementById("withdraw").value = ""
+    //     return alert("please enter correct amount")
+    // }
     if(withdrawValue>balance){
         document.getElementById("withdraw").value = ""
         return alert("you can not withdraw this amount")
@@ -64,14 +95,21 @@ btnwithdraw.addEventListener("click", function () {
     }
 
     //span field 
-    let setwithdraw = document.getElementById("withdraw-amount")
-    let currentWithdraw = setwithdraw.innerText
-    currentWithdraw = parseFloat(currentWithdraw)
+    // let setwithdraw = document.getElementById("withdraw-amount")
+    // let currentWithdraw = setwithdraw.innerText
+    // currentWithdraw = parseFloat(currentWithdraw)
+   
+    currentWithdraw = textToValue("withdraw-amount" , withdrawValue)
+    
+    if(currentWithdraw==undefined){
+        return 
+    }
+   
 
     currentWithdraw = currentWithdraw + withdrawValue
-    setwithdraw.innerText = currentWithdraw
+    document.getElementById("withdraw-amount").innerText = currentWithdraw
 
-  
+    
     balance = balance - withdrawValue
     document.getElementById("balance").innerText = balance
 
